aa38dd25f37f8fa489d9e9281289202770b1571e
lungsi <neuralgraphs@gmail.com>
2019-07-05 08:41:20 +0000

    clarified and cleaned docstrings

--- cerebunit/statistics/data_conditions/forHTmeans.py
+++ cerebunit/statistics/data_conditions/forHTmeans.py
@@ -3,20 +3,32 @@
 #
 # create 3 July 2019 Lungsi
 #
-#
 # =============================================================================
 
 from scipy.stats import normaltest
 
 class NecessaryForHTMeans(object):
-    """doc
+    """
+    Checks for situations for which Hypothesis Testing About Means is valid, i.e, is t-Test valid?
+    ==============================================================================================
+
+    Situation-1
+    -----------
+
+    For large sample sizes, n >= 30, where 30 is the arbitrary demarcation for __large__. This situtation assumes that the population of the measurements (of interest) is not normal.
+
+    Situation-2
+    -----------
+
+    When there is not evidence of extreme outliers or skewed population shape. This is usually the case for population of the measurements that are approximately normal.
+
     """
     @staticmethod
     def check_normal_population(data):
         """Test if sample is from a normal distribution.
         scipy.stats.normaltest is based on D'Agostino & Pearson's omnibus test of normality.
         """
-        alpha = 0.001
+        alpha = 0.001 # an arbitrarily small alpha value
         k2, p = normaltest(data)
         if p < alpha: # null hypothesis: sample comes from normal distribution
             return True
@@ -25,6 +37,8 @@ class NecessaryForHTMeans(object):
 
     @classmethod
     def ask(cls, sample_size, experimental_data):
+        """If the sample size is large the data condition is met otherwise check if the distribution of the raw data is normal.
+        """
         if sample_size >= 30:
             return True
         else:
