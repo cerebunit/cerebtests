675fc47416a76dc0f66dd84173a363e9c140248d
lungsi <neuralgraphs@gmail.com>
2019-08-13 13:20:06 +0000

    added path to cerebunit to pull out docstrings in sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append( os.path.dirpah( os.getcwd() ) )
+sys.path.append( os.path.dirname( os.getcwd() ) )
 
 
 # -- Project information -----------------------------------------------------
