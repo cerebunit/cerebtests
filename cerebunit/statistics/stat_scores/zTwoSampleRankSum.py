45fa9e5df82dae191960b26d77fc7e9fbf045e0d
lungsi <neuralgraphs@gmail.com>
2019-09-18 11:18:52 +0000

    added methods for computing ranks

--- cerebunit/statistics/stat_scores/zTwoSampleRankSum.py
+++ cerebunit/statistics/stat_scores/zTwoSampleRankSum.py
@@ -68,14 +68,21 @@ class ZScoreForTwoSampleRankSumTest(sciunit.Score):
         +============================+================================================+
         | first argument             | dictionary; observation/experimental data      |
         +----------------------------+------------------------------------------------+
-        | second argument            | floating number                                |
+        | second argument            | dictionary; simulated data                     |
         +----------------------------+------------------------------------------------+
 
         *Note:*
 
         * observation **must** have the key "raw_data" whose value is the list of numbers
+        * simulation, i.e, model prediction **must** also have the key "raw_data"
 
         """
+        n1 = len( observation["raw_data"] )
+        n2 = len( prediction["raw_data"] )
+        N = n1 + n2
+        #
+        muW = n1*(1+N)/2
+        sigmaW = np.sqrt( n1*n2*(1+N)/12 )
         data = np.array( observation["raw_data"] )
         splus = ( data < prediction ).sum()
         n_u = (data != prediction ).sum()
@@ -88,4 +95,99 @@ class ZScoreForTwoSampleRankSumTest(sciunit.Score):
 
     def __str__(self):
         return "ZScore is " + str(self.score)
+
+    def get_observation_rank(self, observation, prediction):
+        """Returns ranks for the observation data.
+
+        * sample 1, observation["raw_data"]
+        * sample 2, prediction["raw_data"]
+
+        *Example for describing what 'ranking' means:*
+
+        :math:`sample1 = [65, 60, 62, 70]`
+        :math:`sample2 = [60, 55, 65, 70]`
+        Then,
+        :math:`ordered_data = [55, 60, 60, 62, 65, 65, 70, 70]`
+        :math:`raw_ranks    = [ 1,  2,  3,  4,  5,  6,  7,  8]`
+        and
+        :math:`correct_ranks= [ 1, 2.5, 2.5, 4, 5.5, 5.5, 7.5, 7.5]`
+        Therefore, ranks for sample1 is
+        :math:`sample1_ranks = [5.5, 2.5, 4, 7.5]`
+
+        """
+        ordered_data, all_ranks = self.__orderdata_ranks(observation, prediction)
+        sample1 = np.array( observation["raw_data"] )
+        sample1_ranks = np.zeros((1,len(sample1)))[0]
+        for i in range(len(ordered_data)): # go through all the ordered data
+            a_data = ordered_data[i]
+            its_rank = all_ranks[i]
+            # for each picked data value get its index w.r.t sample1
+            indx_in_sample1 = np.where( sample1 == a_data )[0]
+            if len(indx_in_sample1)>1: # if the picked data value exists within sample1
+                for j in range( len(indx_in_sample1) ): # at each corresponding index in sample1
+                    sample1_ranks[ indx_in_sample1[j] ] = its_rank # set appropriate rank
+        return sample1_ranks
+
+    def __orderdata_ranks(self, observation, prediction):
+        """ Private function that orders the data and returns its appropriate rank.
+
+        * sample 1, observation["raw_data"]
+        * sample 2, prediction["raw_data"]
+
+        **Step-1:**
+
+        * append the two lists (i.e, the two samples)
+        * order the values in ascending manner
+
+        **Step-2:**
+
+        * get unique values in the ordered data
+        * also get the number of frequencies for each unique value
+
+        **Step-3:**
+
+        * construct raw ranks based on the ordered data
+
+        **Step-4:**
+
+        * for each value in the ordered data find its index in unique values array
+        * if the corresponding count is more than one compute its midrank (sum ranks/its count)
+        * set ranks (in raw ranks array) for the corresponding number of values with the computed midrank
+        
+        """
+        ordered_data = np.sort( observation["raw_data"] + prediction["raw_data"] )
+        unique_values, counts = np.unique( ordered_data, return_counts=True )
+        raw_ranks = [ i+1 for i in range(len(ordered_data)) ]
+        #
+        i = 0 # initiate from first index of ordered_data and raw_ranks
+        while i < len(ordered_data):
+            indx_in_uniques = int( np.where( unique_values == ordered_data[i] )[0] )
+            if counts[indx_in_uniques]>1:
+                numer = 0.0
+                numer = [ numer + raw_ranks[i+j] for j in range( counts[indx_in_uniques] ) ][0]
+                for j in range( counts[indx_in_uniques] ):
+                    raw_ranks[i+j] = numer/counts[indx_in_uniques]
+            # raw_ranks[i] does not need to be set for counts = 1
+            i = i + counts[indx_in_uniques] # update loop (skipping repeated values)
+        return [ ordered_data, raw_ranks ] 
+        #sample1 = np.array( observation["raw_data"] )
+        #sample1_ranks = np.zeros((1,len(sample1)))[0]
+        #for i in range(len(ordered_data)):
+        #    a_data = ordered_data[i]
+        #    its_rank = raw_ranks[i]
+        #    indx_in_sample1 = np.where( sample1 == a_data )[0]
+        #    if len(indx_in_sample1)>1:
+        #        for j in range( len(indx_in_sample1) ):
+        #            sample1_ranks[ indx_in_sample1[j] ] = its_rank
+        #return sample1_ranks
+
+
+
+
+
+
+
+
+
+
 # ============================================================================
