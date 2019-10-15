1589203767a4de5a74898a2858e2bdf9dba448e0
lungsi <neuralgraphs@gmail.com>
2019-10-15 13:40:17 +0000

    updated hypothesis testing about medians for single and paired data

--- cerebunit/statistics/hypothesis_testings/signTest.py
+++ cerebunit/statistics/hypothesis_testings/signTest.py
@@ -98,7 +98,7 @@ class HtestAboutMedians:
 
     Using z look up table for standard normal curve which will return its corresponding p.
 
-    ** :math:`^{\dagger}`**
+      :math:`^{\dagger}`
 
     * :math:`\\overrightarrow{x} =` experimental data for one sample testing
     * :math:`\\overrightarrow{x} =` (experimental - prediction) data for paired data testing
