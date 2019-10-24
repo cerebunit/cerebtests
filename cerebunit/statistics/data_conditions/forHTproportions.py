fc6a7d335a24f3e6eafa386f0be6d032078e82e3
lungsi <neuralgraphs@gmail.com>
2019-10-24 14:16:53 +0000

    added large sample condition for chi-sq testing

--- cerebunit/statistics/data_conditions/forHTproportions.py
+++ cerebunit/statistics/data_conditions/forHTproportions.py
@@ -5,12 +5,13 @@
 #
 # =============================================================================
 
-from scipy.stats import normaltest, skew
+import numpy as np
 
 class NecessaryForHTProportions(object):
     """
-    **Checks for situations for which Hypothesis Testing About Proportions is valid, i.e, is t-Test (or standard z-score) valid?**
+    **Checks for situations (sample size requirements) for which Hypothesis Testing About Proportions is valid, i.e, is t-Test (or standard z-score) valid? and also for Hypothesis Testing About proportions by :math:`\\chi^2`-test.**
 
+    **1. For z-test**
 
     **Situation-1**
 
@@ -37,18 +38,37 @@ class NecessaryForHTProportions(object):
     - Ott, R.L. (1998). An Introduction to Statistical Methods and Data Analysis. (p.370) :math:`^{\dagger}`
     - Utts, J.M, Heckard, R.F. (2010). Mind on Statistics. (p.465) :math:`^{\ddagger}`
 
+    **2. For :math:`\\chi^2`-test**
+
+    **Situation-1**
+
+    Dito as above.
+
+    **Situation-2**
+
+    The guidelines for *large sample* are
+
+    - expected values in all the cell of the two-way contingency table should be greater than 1
+    - number of cells with expected values greater than 5 should be **at least** :math:`80\\%` of the total number of cells
+
+    Note that, :math:`\\chi^2`-test may be performed even if the above large sample guidelines are violated but the results *may not* be valid.:math:`^{\dagger\dagger}`
+
+    - Utts, J.M, Heckard, R.F. (2010). Mind on Statistics. (p.588) :math:`^{\dagger\dagger}`
+
     **Implementation**
 
     +-------------------------------------+--------------------------------+
     | Method name                         | Arguments                      |
     +=====================================+================================+
-    | :py:meth:`.ask`                     | experimental_data              |
+    | :py:meth:`.ask_for_ztest`           | n, p0, lb (optional)           |
+    +-------------------------------------+--------------------------------+
+    | :py:meth:`.ask_for_chi2test`        | expected valued two-way table  |
     +-------------------------------------+--------------------------------+
 
     """
     @staticmethod
-    def ask(n, p0, lb=5):
-        """This function checks if the sample size requirement for running hypothesis testing for proportions.
+    def ask_for_ztest(n, p0, lb=5):
+        """This function checks if the sample size requirement for running hypothesis testing for proportions using z-test.
 
         +------------+-------------------------------+
         | Arguments  | Meaning                       |
@@ -86,6 +106,63 @@ class NecessaryForHTProportions(object):
         np0_check = lambda n,p0,lb: True if n*p0 >= lb else False
         n1minusp0_check = lambda n,p0,lb: True if ( n*(1-p0) ) >= lb else False
         if ( np0_check(n, p0, lb) and n1minusp0_check(n, p0, lb) is True ):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def ask_for_chi2test(expected_values):
+        """This function checks if the sample size requirement for running hypothesis testing for proportions using :math:`\\chi^2`-test.
+
+        The argument is the expected values table of the 2 x K two-way contingency table.
+
+        +-------------------+------------------------------------------------------+
+        | Definition        | Meaning                                              |
+        +===================+======================================================+
+        | :math:`n`         | total number of cells in a two-way contingency table |
+        +-------------------+------------------------------------------------------+
+        | :math:`n_1`       | number of expectation values < 1                     |
+        +-------------------+------------------------------------------------------+
+        | :math:`n_5`       | number of expectation values > 5                     |
+        +-------------------+------------------------------------------------------+
+        | :math:`lb_{80\\%}`| :math:`80\\%` of the cells                           |
+        +-------------------+------------------------------------------------------+
+
+        Algorithm that asks if the distribution of an experimental data is normal.
+
+        --------
+
+        |  **Given:** ex; expected values table
+        |  **Get** :math:`n \\leftarrow ex.size`
+        |  **Get** :math:`n_1 \\leftarrow ex[ex<1].size`
+        |  **Get** :math:`n_5 \\leftarrow ex[ex>1].size`
+        |  **Compute** :math:`lb_{80\\%} \\leftarrow ex[ex>1].size`
+        |  **Compute** result2 :math:`\\leftarrow` 0.8 \\times n`
+        |  **if** :math:`n_1 < 0 \\cap n_5 \\geq lb_{80\\%}`
+        |         "sample size requirement is satified"
+        |  **else**
+        |         "sample size requirement is not satisfied"
+
+        --------
+
+        *Note:*
+
+        * boolean return (`True` or `False`)
+        * `True` if sample size requirement is satisfied
+        * `False` if sample size requirement is **not** satisfied
+
+        """
+        #    raise ValueError("question must be normal? or skew?")
+        number_of_expected_counts = expected_values.size
+        number_of_expected_counts_lessthan1 = \
+                    expected_values[ expected_values < 1. ].size
+        number_of_expected_counts_largerthan5 = \
+                    expected_values[ expected_values > 5. ].size
+        lowerbound_80percent = 0.8*number_of_expected_counts
+        #
+        if ( (number_of_expected_counts_lessthan1 < 1) and
+             (number_of_expected_counts_largerthan5 >= lowebound_80percent)
+             is True ):
             return True
         else:
             return False
