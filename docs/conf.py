7f0ec5635376be9bf37f6d6f3cb176bbf1259c4c
lungsi <neuralgraphs@gmail.com>
2019-10-31 10:27:13 +0000

    added statistics definitions

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ html_theme = 'bizstyle'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-html_static_path = ['_static', "/statistics/guides"]
+html_static_path = ['_static', "statistics/guides"]
 
 # -- Options for Notebooks ---------------------------------------------------
 
