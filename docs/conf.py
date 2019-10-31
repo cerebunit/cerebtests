1a2d2616229f797fee60b89f751f34df43ef702f
lungsi <neuralgraphs@gmail.com>
2019-10-31 11:23:30 +0000

    added statistics definitions

--- docs/conf.py
+++ docs/conf.py
@@ -66,9 +66,8 @@ html_theme = 'bizstyle'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-html_static_path = ['_static',
-                    os.getcwd()+os.sep+"statistics"+os.sep+"guides"]
+html_static_path = ['_static']
+html_extra_path = [os.getcwd()+os.sep+"statistics"+os.sep+"guides"]
 #html_extra_path = ["statistics/guides/",
 #                   "statistics/guides/images/",
 #                   "statistics/guides/scripts/"]
